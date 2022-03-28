5eee6fcd2bd3b1420640aa21da12a1a0ea1c3ca2
Carlos Lopez Martinez <69404571+Electroner@users.noreply.github.com>
2022-03-28 22:35:07 +0200

    Add files via upload

---
+++ main.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <exception>
+#include <array>
+#include <string>
+#include <boost/asio/ip/udp.hpp>
+#include <boost/asio/io_context.hpp>
+
+using boost::asio::ip::udp; ///??
+
+const unsigned short HELLO_PORT = 8888;
+const std::string HELLO_PORT_STR{"8888"};
+const std::string HOSTNAME{"127.0.0.1"};
+
+const int MAX_BUFFER_SIZE = 1024;
+
+int main(int argc, char *argv[])
+{
+	boost::asio::io_context io;
+
+	if (argc > 1) // cliente
+	{
+		try
+		{
+			udp::resolver resolver{io};
+			udp::endpoint destination = *resolver.resolve(udp::v4(), HOSTNAME, HELLO_PORT_STR).begin();
+
+			udp::socket socket{io};
+			socket.open(udp::v4());
+
+			std::array<char, MAX_BUFFER_SIZE> sendData = {"Ping!"};
+			std::cout << "Local: Ping!" << std::endl;
+			socket.send_to(boost::asio::buffer(sendData), destination);
+
+			std::array<char, 128> recvData;
+			udp::endpoint sender;
+			size_t len = socket.receive_from(boost::asio::buffer(recvData), sender);
+
+			std::cout.write(recvData.data(), len);
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	else // servidor
+	{
+		try
+		{
+			udp::socket socket{io, udp::endpoint{udp::v4(), HELLO_PORT}};
+			std::cout << "A la espera de conexiones..." << std::endl;
+
+			//while(true)
+			{
+				std::array<char, MAX_BUFFER_SIZE> recvData;
+				udp::endpoint endpoint;
+				boost::system::error_code error;
+
+				socket.receive_from(boost::asio::buffer(recvData), endpoint, 0, error);
+				std::cout.write(recvData.data(), recvData.size());
+				std::cout << std::endl;
+
+				if (error){
+					throw boost::system::system_error(error);
+				}
+				std::string message{"Pong!\n"};
+				std::cout << "Local: Pong!" << std::endl;
+
+				boost::system::error_code ec;
+				socket.send_to(boost::asio::buffer(message), endpoint, 0, ec);
+			}
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << "Excepcion: " << e.what() << std::endl;
+		}
+	}
+}
+
+// g++ -o pingpong main.cc -pthread -lboost_system -lboost_thread -lboost_date_time && ./pingpong
